861ea489725c91e41b4cfdb85e80aaf514e72715
JasonOnes <jasonr.jones14@gmail.com>
2017-06-01 06:59:15 -0700

    Found consistent key from parsed json googlefinance for reliable quote retrieval

--- get_quotes.py
+++ get_quotes.py
@@ -10,28 +10,25 @@ def quote_to_float(fund_name):
 def getQuote(fund_name):
     """retrieves the last price for fund"""
     info = json.dumps(getQuotes(fund_name))
-    #return info[99:105]
-    info_list = info.split()
-    #return info_list[11]
-    """need to turn info into dictionary in case of format changes at googlefinance
-    thus searching by key ie, LastTradePrice"""
-    info_dict = dict(info_list[n:n+2] for n in range(0, len(info_list), 2))
-    print(info_dict.keys())
-    tries = 0
-    while tries < 3:
-        try:
-            return info_dict['"LastTradePrice":']
-            # need to convert quote to float
-        except KeyError:
-            print("try again?")
-            tries += 1
-            #return info_dict['"LastTradePrice":'
-            time.sleep(3)
-        try:
-            return info_dict['"LastTradePrice":']
-            tries += 1
-        except KeyError:
-            pass
+    data = json.loads(info)
+    data_dict = {k: v for d in data for k, v in d.items()}
+    return(data_dict['LastTradePrice'])
+    
+    # tries = 0
+    # while tries < 3:
+    #     try:
+    #         return info_dict['"LastTradePrice":']
+    #         # need to convert quote to float
+    #     except KeyError:
+    #         print("try again?")
+    #         tries += 1
+    #         #return info_dict['"LastTradePrice":'
+    #         time.sleep(3)
+    #     try:
+    #         return info_dict['"LastTradePrice":']
+    #         tries += 1
+    #     except KeyError:
+    #         pass
 
 
         # except KeyError:
@@ -40,9 +37,10 @@ def getQuote(fund_name):
         #     tries += 1
             #getQuote(fund_name)
 
-        """Not sure if this is any better as it's a pretty wonky keyname"""
-    #print(info_dict)
+   
 if __name__ == '__main__':
     print(getQuote('VTSMX'))
+    print(getQuote('VGTSX'))
+    print(getQuote("VGPMX"))
     #print(json.dumps(getQuotes('VTSMX'), indent=2))
-    #print(quote_to_float('VTSMX'))
+    print((quote_to_float('VTSMX')))
